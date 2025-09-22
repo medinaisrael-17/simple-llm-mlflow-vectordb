edcfd0394e479e7daa01680cfb42dab409c2c611
medinaisrael-17 <medinaisrael_17@yahoo.com>
2025-09-22 16:03:20 -0500

    our SimpleLLM

--- src/model.py
+++ src/model.py
@@ -1,1 +1,10 @@
 # Defines LLM wrapper class
+
+from transformers import pipeline
+
+class SimpleLLM: 
+    def __init__(self, model_name="distilgpt2"):
+        self.generator = pipeline("text-generation", model=model_name)
+
+    def generate(self, prompt, max_length=50):
+        return self.generator(prompt, max_length=max_length, num_return_sequence=1)[0]["generated_text"]
