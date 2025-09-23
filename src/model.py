02564cb1043c808221fbfa18437b486e71af9334
medinaisrael-17 <medinaisrael_17@yahoo.com>
2025-09-22 20:29:52 -0500

    changing model to google/flan-t5-small to get correct answers back from our LLM

--- src/model.py
+++ src/model.py
@@ -3,8 +3,8 @@
 from transformers import pipeline
 
 class SimpleLLM: 
-    def __init__(self, model_name="distilgpt2"):
-        self.generator = pipeline("text-generation", model=model_name)
+    def __init__(self, model_name="google/flan-t5-small"):
+        self.generator = pipeline("text2text-generation", model=model_name)
 
     def generate(self, prompt, max_length=50):
-        return self.generator(prompt, max_length=max_length, num_return_sequence=1)[0]["generated_text"]
+        return self.generator(prompt, max_length=max_length, num_return_sequences=1)[0]["generated_text"]
