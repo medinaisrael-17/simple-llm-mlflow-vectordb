9f2445973abf3fc0cbd1a523e18deb490114259c
medinaisrael-17 <medinaisrael_17@yahoo.com>
2025-09-22 22:53:31 -0500

    Adding in notes

--- src/model.py
+++ src/model.py
@@ -7,4 +7,11 @@ class SimpleLLM:
         self.generator = pipeline("text2text-generation", model=model_name)
 
     def generate(self, prompt, max_length=50):
+        # max_length limits how many tokens the model will generate to prevent
+        # infitely running and creating long outputs.
+        # 
+        # num_return_sequences will return only one answer. Increasing the number woll increase the amount
+        # of possible answers returned in a List of dicts. 
+        #
+        # The rest is List traversal
         return self.generator(prompt, max_length=max_length, num_return_sequences=1)[0]["generated_text"]
--- src/vector_store.py
+++ src/vector_store.py
@@ -5,18 +5,24 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 class VectorDB:
+    # dim is the dimension of our vectors, or how long each embedding is.
+    # our FAISS needs to know this in order to store and compare vectors correctly. 
     def __init__(self, embedding_model="all-MiniLM-L6-v2", dim=384):
         self.embedder = SentenceTransformer(embedding_model)
+        # Flat means it stores all vectors in memory in a simple, linear array.
+        # No fancy trees or clustering.
+        # L2 refers to a distance metric used L2 = Euclidean distance. 
+        # Finds what is closest in Eucledian Space. 
         self.index = faiss.IndexFlatL2(dim)
         self.documents = []
 
     def add(self, docs):
         embeddings = self.embedder.encode(docs)
-        self.index.add(np.array(embeddings))
+        self.index.add(np.array(embeddings)) # add our document vectors.
         self.documents.extend(docs)
 
     def query(self, text, top_k=3):
         embedding = self.embedder.encode([text])
-        D, I = self.index.search(np.array(embedding), top_k)
+        D, I = self.index.search(np.array(embedding), top_k) # D = distance to the closest vectors I = indicies of the closest vectores in stored data.
         return [self.documents[i] for i in I[0]]
 
